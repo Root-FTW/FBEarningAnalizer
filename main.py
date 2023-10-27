aa18e6da4d1dd4459a831362c43c18a443d6f232
RootFTW <108767514+Root-FTW@users.noreply.github.com>
2023-10-26 22:07:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,46 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 from datetime import datetime, timedelta
+import streamlit as st
 
-# Load the data
-file_path = 'C:/Users/rootf/Downloads/Jul-27-2023_Oct-26-2023_1529436154481158.csv'
-data = pd.read_csv(file_path)
+def main():
+    st.title('Análisis de Video')
+    st.write('Por favor, sube un archivo CSV con información de video para la analítica')
 
-# Convert the 'Hora de publicación' column to datetime
-data['Hora de publicación'] = pd.to_datetime(data['Hora de publicación'], format='%m/%d/%Y %H:%M')
+    data_upload = st.file_uploader("Seleccione un archivo CSV", type="csv")
 
-# Get the current date and time
-now = datetime.now()
+    if data_upload is not None:
+        data = load_data(data_upload)
+        process_data(data)
+        st.success('Análisis completado!')
 
-# Calculate the cutoff date (5 days ago)
-cutoff = now - timedelta(days=5)
+def load_data(file):
+    # Carga el archivo CSV a un dataframe
+    data = pd.read_csv(file)
 
-# Filter the DataFrame
-data = data[data['Hora de publicación'] < cutoff]
+    # Formatea 'Hora de publicación' como valor datetime
+    data['Hora de publicación'] = pd.to_datetime(data['Hora de publicación'], format='%m/%d/%Y %H:%M')
 
-# Group the data and sum the earnings
-data_grouped = data.groupby(['Nombre de la página', 'Identificador del activo de video', 'Hora de publicación'])['Ingresos estimados (USD)'].sum().reset_index()
+    return data
 
-# Filter rows with zero earnings
-data_grouped = data_grouped[data_grouped['Ingresos estimados (USD)'] == 0]
+def process_data(data):
+    # Obtiene la fecha/hora actual
+    now = datetime.now()
 
-# Export the data to a new CSV file
-data_grouped.to_csv('C:/Users/rootf/Downloads/videos_earnings.csv', index=False)
+    # Calcula la fecha de corte (5 días atrás)
+    cutoff = now - timedelta(days=5)
+
+    # Filtra el dataframe
+    data = data[data['Hora de publicación'] < cutoff]
+
+    # Agrupa y suma los ingresos
+    data_grouped = data.groupby(['Nombre de la página', 'Identificador del activo de video', 'Hora de publicación'])['Ingresos estimados (USD)'].sum().reset_index()
+
+    # Filtra registros con cero ingresos
+    data_grouped = data_grouped[data_grouped['Ingresos estimados (USD)'] == 0]
+
+    # Muestra los datos
+    st.dataframe(data_grouped)
+
+if __name__ == "__main__":
+    main()
