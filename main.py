881e43e6854dc2920ba1ee49bc82eff4721bf8e5
RootFTW <108767514+Root-FTW@users.noreply.github.com>
2023-12-27 17:58:57 -0800

    Update main.py

--- main.py
+++ main.py
@@ -17,14 +17,23 @@ def main():
         # Agrega un filtro para seleccionar el nombre de la página
         unique_pages = data['Nombre de la página'].unique()
         selected_page = st.selectbox('Filtrar por nombre de la página:', ['Todos'] + list(unique_pages))
-        
+
         if selected_page != 'Todos':
             data = data[data['Nombre de la página'] == selected_page]
-        
-        processed_data = process_data(data)
-        st.success('Análisis completado!')
-        # Utiliza todo el ancho de la página para el DataFrame
-        st.dataframe(processed_data, width=1500)
+
+        # Agrega filtros para 'Veces que se compartió' y 'Reproducciones de video de 3 segundos'
+        min_shares = st.number_input('Número mínimo de veces que se compartió:', min_value=0, value=0, step=1)
+        max_shares = st.number_input('Número máximo de veces que se compartió (0 para sin límite):', min_value=0, value=0, step=1)
+        min_views = st.number_input('Número mínimo de reproducciones de video de 3 segundos:', min_value=0, value=0, step=1)
+        max_views = st.number_input('Número máximo de reproducciones de video de 3 segundos (0 para sin límite):', min_value=0, value=0, step=1)
+
+        if st.button('Aplicar filtros'):
+            # Aplica los nuevos filtros
+            data = apply_filters(data, min_shares, max_shares, min_views, max_views)
+            processed_data = process_data(data)
+            st.success('Análisis completado!')
+            # Utiliza todo el ancho de la página para el DataFrame
+            st.dataframe(processed_data, width=1500)
 
 def load_data(file):
     # Carga el archivo CSV a un dataframe
@@ -32,6 +41,21 @@ def load_data(file):
 
     # Formatea 'Hora de publicación' como valor datetime
     data['Hora de publicación'] = pd.to_datetime(data['Hora de publicación'], format='%m/%d/%Y %H:%M')
+
+    return data
+
+def apply_filters(data, min_shares, max_shares, min_views, max_views):
+    # Aplica el filtro para 'Veces que se compartió'
+    if max_shares > 0:  # Asume que un valor de 0 significa sin límite superior
+        data = data[(data['Veces que se compartió'] >= min_shares) & (data['Veces que se compartió'] <= max_shares)]
+    else:
+        data = data[data['Veces que se compartió'] >= min_shares]
+
+    # Aplica el filtro para 'Reproducciones de video de 3 segundos'
+    if max_views > 0:  # Asume que un valor de 0 significa sin límite superior
+        data = data[(data['Reproducciones de video de 3 segundos'] >= min_views) & (data['Reproducciones de video de 3 segundos'] <= max_views)]
+    else:
+        data = data[data['Reproducciones de video de 3 segundos'] >= min_views]
 
     return data
 
@@ -52,13 +76,9 @@ def process_data(data):
         'Reproducciones de video de 3 segundos': 'sum'
     }).reset_index()
 
-    # Filtra para mostrar solo registros donde los ingresos estimados son igual a cero
-    data_grouped = data_grouped[data_grouped['Ingresos estimados (USD)'] == 0]
-
-    # Convert the 'Identificador del activo de video' column to String
-    data_grouped['Identificador del activo de video'] = data_grouped['Identificador del activo de video'].astype(str)
+    # Filtra para mostrar solo registros donde los ingresos estimados son mayores a cero
+    data_grouped = data_grouped[data_grouped['Ingresos estimados (USD)'] > 0]
 
     return data_grouped
 
-if __name__ == "__main__":
-    main()
+if __name__ ==
