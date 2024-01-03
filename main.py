a64c3ba0a85dc94d3628a5163c073d507138c8fc
RootFTW <108767514+Root-FTW@users.noreply.github.com>
2024-01-03 14:25:16 -0800

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,12 @@ def main():
 
     if data_upload is not None:
         data = load_data(data_upload)
-        # Filtro para seleccionar el nombre de la página (opcional, dependiendo de si aún quieres este filtro)
+        # Filtro para seleccionar el nombre de la página
         unique_pages = data['Nombre de la página'].unique()
         selected_page = st.selectbox('Filtrar por nombre de la página:', ['Todos'] + list(unique_pages))
         
         # Filtros adicionales con valores mínimos y máximos
         min_shares, max_shares = st.slider('Filtrar por el número de veces que se compartió:', 0, int(data['Veces que se compartió'].max()), (0, int(data['Veces que se compartió'].max())))
-        # Establecemos un umbral para considerar ingresos como cero
         ingresos_minimos_para_considerar = 0.0001
         min_earnings, max_earnings = st.slider('Filtrar por el rango de ingresos estimados (USD):', 0.0, float(data['Ingresos estimados (USD)'].max()), (0.0, float(data['Ingresos estimados (USD)'].max())))
         min_views, max_views = st.slider('Filtrar por el rango de reproducciones de video de 3 segundos:', 0, int(data['Reproducciones de video de 3 segundos'].max()), (0, int(data['Reproducciones de video de 3 segundos'].max())))
@@ -30,13 +29,11 @@ def main():
             data = data[data['Nombre de la página'] == selected_page]
 
         data = data[(data['Veces que se compartió'] >= min_shares) & (data['Veces que se compartió'] <= max_shares)]
-        # Modificamos el filtro para considerar el umbral de ingresos
         data = data[((data['Ingresos estimados (USD)'] >= min_earnings) & (data['Ingresos estimados (USD)'] <= max_earnings)) | (data['Ingresos estimados (USD)'] < ingresos_minimos_para_considerar)]
         data = data[(data['Reproducciones de video de 3 segundos'] >= min_views) & (data['Reproducciones de video de 3 segundos'] <= max_views)]
 
         processed_data = process_data(data)
         st.success('Análisis completado!')
-        # Aseguramos que la visualización muestre la precisión correcta
         st.dataframe(processed_data.style.format({'Ingresos estimados (USD)': '{:.4f}'}))
 
 def load_data(file):
@@ -58,8 +55,8 @@ def process_data(data):
     # Filtra el dataframe para incluir solo registros antes de la fecha de corte
     data = data[data['Hora de publicación'] < cutoff]
 
-    # Agrupa y suma los valores de las tres columnas de interés solo por 'Identificador del activo de video'
-    data_grouped = data.groupby('Identificador del activo de video').agg({
+    # Agrupa y suma los valores de las tres columnas de interés por 'Nombre de la página', 'Identificador del activo de video', y 'Hora de publicación'
+    data_grouped = data.groupby(['Nombre de la página', 'Identificador del activo de video', 'Hora de publicación']).agg({
         'Ingresos estimados (USD)': 'sum',
         'Veces que se compartió': 'sum',
         'Reproducciones de video de 3 segundos': 'sum'
